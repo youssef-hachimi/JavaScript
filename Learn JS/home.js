03e2a6074bb3eed49bbacc203d03b758c4a69dd2
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-12-13 18:05:19 +0100

    Add files via upload

---
+++ Learn JS/home.js
@@ -0,0 +1,114 @@
+/*
+primitive values 
+42 | 3.14 | "holly" | true false | undefined |null
+number | string | boolean | undefined | Null
+complex Date : Array | object
+array : [34,56,66] , zero indexed : [0,1,2]
+object : {age : 19} ,{Fname : "youssef"} property key name(age) , value(19) you can also type like this : {"age : 19"}
+{
+    age : 19,
+    firstName : "youssef"
+}
+// operations
+Binary Operation : + - / * % Ex: 3 + 37
+string Concatenation :
+"youssef" + "hachimi" = youssef hahcimi
+*/
+// Numbers arithmetic
+console.log(15 + 3); // 18
+console.log(30 / 3); // 10
+console.log(5 * 3); // 15
+console.log(30 % 3); // 0
+console.log(15 - 4); // 11
+// Concatenation 
+console.log("youssef" + " hachimi")//youssef hachimi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
